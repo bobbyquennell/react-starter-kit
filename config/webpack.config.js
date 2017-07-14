5ae56a908fb0086f5c6669751b67fbed4506b207
bobbyquennell <bobbyquennell@gmail.com>
2017-07-14 23:16:06 +1000

    add support of loading images

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,15 +4,22 @@ module.exports = {
     entry: './src/index.js',
     output:{
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '../dist/'
     },
     module:{
         rules:[
             {
-                test:/\.css$/,
+                test: /\.css$/,
                 use:[
                     'style-loader',
                     'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use:[
+                    'file-loader'
                 ]
             }
         ]
