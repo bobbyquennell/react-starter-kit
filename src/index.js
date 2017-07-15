cd07291844e3b4eb04a09438bb1c16b23e915b0e
bobbyquennell <bobbyquennell@gmail.com>
2017-07-15 20:08:45 +1000

    add support to loading fonts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-
+import  './style.css';
+import Icon from '../public/icon.png';
 function component(){
     var element = document.createElement('div');
     element.innerHTML = 'hello world';
+    element.classList.add('hello');
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
     return element;
 }
 
