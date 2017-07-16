9a119287bb3a22a6c15e8181bd5cb1eba1bb39b7
bobbyquennell <bobbyquennell@gmail.com>
2017-07-16 22:59:19 +1000

    add support to output multiple bundle js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import  './style.css';
+import printMe from './print.js';
 import Icon from '../public/icon.png';
 function component(){
     var element = document.createElement('div');
+    var btn = document.createElement('button');
     element.innerHTML = 'hello world';
     element.classList.add('hello');
     var myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
