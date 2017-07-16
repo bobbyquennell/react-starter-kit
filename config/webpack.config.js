bda7c5fcbbad19dad38e024a99a39746694db0d9
bobbyquennell <bobbyquennell@gmail.com>
2017-07-16 23:39:03 +1000

    add support to clean build/dist folder

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,9 +3,18 @@ var path = require('path');
 //see link below for more details
 //https://webpack.js.org/guides/output-management/#setting-up-htmlwebpackplugin
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// the path(s) that should be cleaned
+var pathsToClean = [
+  'dist'
+]
+// the clean options to use
+var cleanOptions = {
+  root:    path.resolve(__dirname, '../')
+}
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     entry: {
-        main:'./src/index.js',
+        app:'./src/index.js',
         print:'./src/print.js'
     },
     output:{
@@ -17,7 +26,8 @@ module.exports = {
         // Create HTML file that includes references to bundled CSS and JS.
         new HtmlWebpackPlugin({
             title: 'Output Management'
-        })
+        }),
+        new CleanWebpackPlugin(pathsToClean, cleanOptions)
     ],
     module:{
         rules:[
