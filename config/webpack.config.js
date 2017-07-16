e5ef5bde7b6961963074673f5ec1c2590bc322bb
bobbyquennell <bobbyquennell@gmail.com>
2017-07-16 23:15:06 +1000

    add support to generate index.html automatically

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,8 +1,11 @@
 var path = require('path');
-
+//add support to create index.html automatically base on the ouput configuration
+//see link below for more details
+//https://webpack.js.org/guides/output-management/#setting-up-htmlwebpackplugin
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
-        app:'./src/index.js',
+        main:'./src/index.js',
         print:'./src/print.js'
     },
     output:{
@@ -10,6 +13,12 @@ module.exports = {
         path: path.resolve(__dirname, '../dist'),
         publicPath: '../dist/'
     },
+    plugins:[
+        // Create HTML file that includes references to bundled CSS and JS.
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     module:{
         rules:[
             {
